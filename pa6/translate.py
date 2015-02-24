b6b0d288ab0c22cdb1ef8b80ddeb236246cf9b6f
erichermann13 <erichermann13@gmail.com>
2015-02-23 22:37:43 -0800

    cleaned up

--- pa6/translate.py
+++ pa6/translate.py
@@ -42,35 +42,26 @@ def run_complex_translation():
   translations = json.load(translation_file)
   count = 1
   f = open('Corpus.txt', 'r')
-
+  # This is from the NLTK toolkit
   cess_sents = cess_esp.tagged_sents()
-  cess_esp.tagged_sents()
   unigramTagger = UnigramTagger(cess_sents)
 
   for line in f:
-    line = line.decode("utf8")
-    line = line.strip('\n')
-    # code modified from stackoverflow:
 
     words_in_line = nltk.word_tokenize(line)
     print words_in_line
     taggedWords = unigramTagger.tag(words_in_line)
-    output = ""
-    for word in taggedWords:
-      # output = output + " "
-      for thing in word:
-        # output = output + thing
-        print thing
-      output = output + " "
-    print output
-    # translated_sentence = []
-    # for word in words_in_line:
-    #   if word in translations:
-    #     translated_sentence.append(translations[word])
-    #   else:
-    #     translated_sentence.append(word)
-    # translated_sentence = ' '.join(translated_sentence)
-    # print str(count) + '. ' + translated_sentence
+
+    for index in xrange(0,len(taggedWords)):
+      currWord = taggedWords[index]
+      print currWord[0]
+      tag = currWord[1]
+      print tag
+      if index != len(taggedWords)-1:
+        nextWord = taggedWords[index+1]
+        if nextWord[0] == "hello":
+          pass
+
     count += 1
 
 
