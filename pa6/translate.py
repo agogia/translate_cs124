85e5c30f6e781ddf9b7b5639bbdaf26cb1081b17
erichermann13 <erichermann13@gmail.com>
2015-02-23 20:15:21 -0800

    added variable to represent running without or with changes

--- pa6/translate.py
+++ pa6/translate.py
@@ -1,30 +1,29 @@
-#Eric test comment
-
-
-
 import json
 
 def main():
-  translation_file = open('translations.json', 'r')
-  translations = json.load(translation_file)
-  count = 1
-  f = open('Corpus.txt', 'r')
-  for line in f:
-    line = line.decode("utf8")
-    line = line.strip('\n')
-    words_in_line = line.split(' ')
-    translated_sentence = []
-    for word in words_in_line:
-      if word in translations:
-        translated_sentence.append(translations[word])
-      else:
-        translated_sentence.append(word)
-    translated_sentence = ' '.join(translated_sentence)
-    print str(count) + '. ' + translated_sentence
-    count += 1
 
-    dummyVar = 27
+  run_translation(False)
+  
 
+def run_translation(withChanges):
+  if withChanges==False:
+    translation_file = open('translations.json', 'r')
+    translations = json.load(translation_file)
+    count = 1
+    f = open('Corpus.txt', 'r')
+    for line in f:
+      line = line.decode("utf8")
+      line = line.strip('\n')
+      words_in_line = line.split(' ')
+      translated_sentence = []
+      for word in words_in_line:
+        if word in translations:
+          translated_sentence.append(translations[word])
+        else:
+          translated_sentence.append(word)
+      translated_sentence = ' '.join(translated_sentence)
+      print str(count) + '. ' + translated_sentence
+      count += 1
 
 
 if __name__ == '__main__':
