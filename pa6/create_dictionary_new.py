acb4220b18804aaec82cb8f210cc2b1fd2e9ea1d
erichermann13 <erichermann13@gmail.com>
2015-02-24 22:26:50 -0800

    created new dict

--- pa6/create_dictionary_new.py
+++ pa6/create_dictionary_new.py
@@ -4,24 +4,24 @@ def main():
 	f = open('DictionaryTextFile.txt', 'r')
 	count = 0
 	dictionary = collections.defaultdict(list)
-  	for line in f:
-  		wordList = []
-  		colonIndex = str.find(line, ":")
-  		foreignWord = line[0:colonIndex]
-  		line = line[colonIndex+2:]
-  		while (True):
-  			commaIndex = str.find(line, ",")
-  			if(commaIndex > 0):
-  				englishWord = line[0:commaIndex]
-  				line = line[commaIndex+2:]
-  				wordList.append(englishWord)
-  			else:
-  				englishWord = line[0:len(line)-1]
-  				wordList.append(englishWord)
-  				break
-  		dictionary[foreignWord] = wordList
+	for line in f:
+		wordList = []
+		colonIndex = str.find(line, ":")
+		foreignWord = line[0:colonIndex]
+		line = line[colonIndex+2:]
+		while (True):
+			commaIndex = str.find(line, ",")
+			if(commaIndex > 0):
+				englishWord = line[0:commaIndex]
+				line = line[commaIndex+2:]
+				wordList.append(englishWord)
+			else:
+				englishWord = line[0:len(line)-1]
+				wordList.append(englishWord)
+				break
+		dictionary[foreignWord] = wordList
 
-  	print(dictionary["caja"])
+    print(dictionary["caja"])
   	
 
 
--- pa6/translate.py
+++ pa6/translate.py
@@ -1,5 +1,7 @@
 import json
 import nltk
+import sys
+import collections
 from nltk.corpus import cess_esp
 from nltk import UnigramTagger
 
@@ -38,31 +40,52 @@ def run_translation(withChanges):
 
 
 def run_complex_translation():
-  translation_file = open('translations.json', 'r')
-  translations = json.load(translation_file)
-  count = 1
-  f = open('Corpus.txt', 'r')
-  # This is from the NLTK toolkit
-  cess_sents = cess_esp.tagged_sents()
-  unigramTagger = UnigramTagger(cess_sents)
+  create_new_dict()
+  # translation_file = open('translations.json', 'r')
+  # translations = json.load(translation_file)
+  # count = 1
+  # f = open('Corpus.txt', 'r')
+  # # This is from the NLTK toolkit
+  # cess_sents = cess_esp.tagged_sents()
+  # unigramTagger = UnigramTagger(cess_sents)
 
+  # for line in f:
+
+  #   words_in_line = nltk.word_tokenize(line)
+  #   print words_in_line
+  #   taggedWords = unigramTagger.tag(words_in_line)
+
+  #   for index in xrange(0,len(taggedWords)):
+  #     currWord = taggedWords[index]
+  #     print currWord[0]
+  #     tag = currWord[1]
+  #     print tag
+  #     if index != len(taggedWords)-1:
+  #       nextWord = taggedWords[index+1]
+  #       if nextWord[0] == "hello":
+  #         pass
+
+  #   count += 1
+
+def create_new_dict():
+  f = open('DictionaryTextFile.txt', 'r')
+  dictionary = collections.defaultdict(list)
   for line in f:
-
-    words_in_line = nltk.word_tokenize(line)
-    print words_in_line
-    taggedWords = unigramTagger.tag(words_in_line)
-
-    for index in xrange(0,len(taggedWords)):
-      currWord = taggedWords[index]
-      print currWord[0]
-      tag = currWord[1]
-      print tag
-      if index != len(taggedWords)-1:
-        nextWord = taggedWords[index+1]
-        if nextWord[0] == "hello":
-          pass
-
-    count += 1
+    wordList = []
+    colonSplit = line.split(": ")
+    spanishWord = colonSplit[0]
+    translations = colonSplit[1]
+    while (True):
+      commaIndex = str.find(translations, ",")
+      if(commaIndex > 0):
+        englishWord = translations[0:commaIndex]
+        translations = translations[commaIndex+2:]
+        wordList.append(englishWord)
+      else:
+        englishWord = translations[0:len(translations)-1]
+        wordList.append(englishWord)
+        break
+    dictionary[spanishWord] = wordList
 
 
 if __name__ == '__main__':
